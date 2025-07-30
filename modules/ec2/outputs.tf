35e8956f439aed27e692f4955898c0ab25e1fa72
Michal-okr <okrom87@gmail.com>
2025-07-30 22:19:22 +0200

    nahrani

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -12,8 +12,3 @@ output "public_dns" {
   description = "Veřejný DNS název EC2 instance"
   value       = aws_instance.ec2_instance.public_dns
 }
-
-output "ssh_command" {
-  description = "Příkaz pro SSH připojení k instanci"
-  value       = "ssh -i ${var.public_key_path} ec2-user@${aws_instance.ec2_instance.public_ip}"
-}
